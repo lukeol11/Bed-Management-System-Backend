f3321913c1a482373efa4403ecc0239858b20fa2
Luke O'Leary <luke.oleary1@gmail.com>
2024-02-19 15:30:29 +0000

    feat: added dto

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -5,29 +5,42 @@ import {
     IsEmail,
     IsDate
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateUserDto {
+export class UserDto {
+    @ApiProperty()
+    @IsNumber()
+    id: number;
+
+    @ApiProperty()
     @IsString()
     first_name: string;
 
+    @ApiProperty()
     @IsString()
     last_name: string;
 
+    @ApiProperty()
     @IsBoolean()
     can_approve_requests: boolean;
 
+    @ApiProperty()
     @IsNumber()
     hospital_id: number;
 
+    @ApiProperty()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     phone_number: string;
 
+    @ApiProperty()
     @IsDate()
     created_at: Date;
 
+    @ApiProperty()
     @IsNumber()
     created_by: number;
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Users } from './entities/users.entity';
+import { UserDto } from './dto/users.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('/api/users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
     @Get('/all')
+    @ApiResponse({
+        status: 200,
+        description: 'Get all users',
+        type: UserDto,
+        isArray: true
+    })
     async getAllUsers(): Promise<Users[]> {
         return this.usersService.findAll();
     }
