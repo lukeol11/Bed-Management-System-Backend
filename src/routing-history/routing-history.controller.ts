6a7c0295016168bd683a8d04c6722bcedb24db4e
Luke O'Leary <luke.oleary1@gmail.com>
2024-06-13 15:21:39 +0100

    feat: added get routing history endpoint

--- src/routing-history/routing-history.controller.ts
+++ src/routing-history/routing-history.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RoutingHistoryService } from './routing-history.service';
 import { CreateRoutingHistoryDto } from './dto/createRoutingHistory';
@@ -21,5 +21,28 @@ export class RoutingHistoryController {
         @Body() routingHistory: CreateRoutingHistoryDto
     ): Promise<RoutingHistory> {
         return this.routingHistoryService.addRoutingHistory(routingHistory);
+    }
+
+    @Get('/routing-history')
+    @ApiResponse({
+        status: 200,
+        description: 'Get routing history',
+        type: RoutingHistory,
+        isArray: true
+    })
+    async getRoutingHistory(
+        @Query('startDate') startDate: string,
+        @Query('endDate') endDate: string,
+        @Query('userId') userId: string
+    ): Promise<RoutingHistory[]> {
+        const parsedStartDate = new Date(startDate);
+        const parsedEndDate = new Date(endDate);
+        const parsedUserId = parseInt(userId, 10);
+
+        return this.routingHistoryService.getRoutingHistory(
+            parsedStartDate,
+            parsedEndDate,
+            parsedUserId
+        );
     }
 }
--- src/routing-history/routing-history.service.ts
+++ src/routing-history/routing-history.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { RoutingHistory } from './entities/routing-history.entity';
 import { CreateRoutingHistoryDto } from './dto/createRoutingHistory';
 import { Repository } from 'typeorm';
+import { Between } from 'typeorm';
 
 @Injectable()
 export class RoutingHistoryService {
@@ -15,5 +16,20 @@ export class RoutingHistoryService {
         routingHistory: CreateRoutingHistoryDto
     ): Promise<RoutingHistory> {
         return this.routingHistoryRepository.save(routingHistory);
+    }
+
+    async getRoutingHistory(
+        startDate: Date,
+        endDate: Date,
+        userId: number
+    ): Promise<RoutingHistory[]> {
+        const results = await this.routingHistoryRepository.find({
+            where: {
+                timestamp: Between(startDate, endDate),
+                user_id: userId
+            }
+        });
+
+        return results;
     }
 }
