382331c6cef4f8368c0867261f1118c6ba6a9066
Luke O'Leary <luke.oleary1@gmail.com>
2024-06-19 09:32:13 +0100

    fix: removed unused endpoint

--- src/hospitals/hospitals.controller.ts
+++ src/hospitals/hospitals.controller.ts
@@ -1,16 +1,9 @@
-import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Query
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { HospitalsService } from './hospitals.service';
 import { HospitalDto } from './dto/hospital.dto';
 import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Hospital } from './entities/hospital.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Controller('/api/hospitals')
 @ApiTags('hospitals')
@@ -42,15 +35,4 @@ export class HospitalsController {
     async getHospitalById(@Query('id') id: number): Promise<HospitalDto> {
         return this.hospitalsService.findHospitalById(id);
     }
-
-    @Delete('/delete/:id')
-    @ApiResponse({
-        status: 200,
-        description: 'Delete hospital by ID',
-        type: Hospital
-    })
-    async deleteHospital(@Param('id') id: number): Promise<string> {
-        return this.hospitalsService.deleteHospital(id);
-    }
 }
-
